905225d2f95fc87597ea838804e2d66559b0326c
Shahnawaz1990 <shahnawaz.nazimali@gmail.com>
2023-10-16 12:54:22 +0500

    final

---
+++ app/properties/page.tsx
@@ -0,0 +1,40 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+import PropertiesClient from "./PropertiesClient";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getListings from "../actions/getListings";
+
+const PropertiesPage = async () => {
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser){
+        return(
+            <ClientOnly>
+                <EmptyState title="Unauthorized" subtitle="Please Login" />
+            </ClientOnly>
+        )
+    }
+
+    const listings = await getListings({
+        userId: currentUser.id
+    });
+
+    if(listings.length === 0){
+        return(
+            <ClientOnly>
+                <EmptyState title="No Properties Found" subtitle="Lookes Like you havent reserved no properties." />
+            </ClientOnly>
+        )
+    }
+
+    return (
+        <ClientOnly>
+                <PropertiesClient
+                listings={listings} currentUser={currentUser}
+                />
+            </ClientOnly>
+    )
+}
+
+export default PropertiesPage;
